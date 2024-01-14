4be7dfebfe514548f2bbb36cfbde767f531c880b
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-14 14:11:26 +0530

    anagram

---
+++ Problem242.java
@@ -0,0 +1,28 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+import java.util.Arrays;
+
+class Problem242 {
+    static boolean isAnagram(String s, String t) {
+        char[] charS = s.toCharArray();
+        char[] charT = t.toCharArray();
+        Arrays.sort(charS);
+        Arrays.sort(charT);
+        String ss = new String(charS);
+        String st = new String(charT);
+        return ss.equals(st);
+    }
+
+    public static void main(String[] args) {
+        String s = "rat";
+        String t = "car";
+        System.out.println(isAnagram(s, t));
+    }
+}
