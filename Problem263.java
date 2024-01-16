2e2bc883fb37d9e06f351926f82bd15ad59d975d
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-16 18:47:04 +0530

    ugly number - prob 263

---
+++ Problem263.java
@@ -0,0 +1,28 @@
+class Problem263 {
+    static boolean isUgly(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n % 5 == 0) {
+            n = n / 5;
+        }
+        while (n % 3 == 0) {
+            n = n / 3;
+        }
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        if (n == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int n = 0;
+        boolean bool = isUgly(n);
+
+        System.out.println(bool);
+    }
+}
---
+++ ToDo/Problem67.java
@@ -0,0 +1,16 @@
+package ToDo;
+class Problem67 {
+    static String addBinary(String a, String b) {
+        
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String a = "11";
+        String b = "1";
+
+        String s = addBinary(a, b);
+        System.out.println("result: " + s + " " + s.length());
+    }
+}
