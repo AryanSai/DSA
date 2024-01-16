c57ca90aa8f3f994fc7fc82386a24f0cbc9b86b5
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-16 20:15:51 +0530

    reverse string - prob 344

---
+++ Problem344.java
@@ -0,0 +1,21 @@
+class Problem344 {
+    static void reverseString(char[] s) {
+        int i = 0, j = s.length - 1;
+        char temp;
+        while (i <= j) {
+            System.out.println(s[i]);
+            System.out.println(s[j]);
+            temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        char[] s = { 'h' };
+        reverseString(s);
+        System.out.println(s);
+    }
+}
