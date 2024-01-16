a7a3bc3bbfd63305e198449b7fde2e004e2ec114
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-16 19:08:08 +0530

    missing number - prob 268

---
+++ Problem268.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Problem268 {
+    static int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int i;
+        for (i = 0; i < nums.length; i++) {
+            if (i != nums[i]) {
+                return i;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 0, 1 };
+
+        System.out.println(missingNumber(nums));
+    }
+}
