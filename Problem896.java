9bbef5ef06c7ab5dbc20d5feb4830d9da8b1edc3
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-01-22 18:41:07 +0530

    monotonic array

---
+++ Problem896.java
@@ -0,0 +1,19 @@
+class Problem896 {
+    static boolean isMonotonic(int[] nums) {
+        int inc = 0, dec = 0;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i - 1] <= nums[i]) {
+                inc++;
+            }
+            if (nums[i - 1] >= nums[i]) {
+                dec++;
+            }
+        }
+        return (inc == nums.length - 1) || (dec == nums.length - 1);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = { 1, 1 };
+        System.out.println(isMonotonic(nums));
+    }
+}
