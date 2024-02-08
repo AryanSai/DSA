425bf434c3c0f56712e89e7d1b1b79ef21d59592
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-02-08 18:45:32 +0530

    520 Capital

--- ToDO/Problem739.java
+++ ToDO/Problem739.java
@@ -1,6 +1,9 @@
 package ToDO;
 // 739. Daily Temperatures (Medium)
-// Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+// Given an array of integers temperatures represents the daily temperatures, return an array answer such 
+// that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If 
+// there is no future day for which this is possible, keep answer[i] == 0 instead.
 // Example 1:
 // Input: temperatures = [73,74,75,71,69,72,76,73]
 // Output: [1,1,4,2,1,1,0,0]
@@ -17,7 +20,7 @@ class Problem739 {
     static int[] dailyTemperatures(int[] temp) {
         int[] answer = new int[temp.length];
         // Stack<Integer> stack = new Stack<>();
-        
+
         return answer;
     }
 
