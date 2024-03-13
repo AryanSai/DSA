f856dd66753c8b23a6cfcba9de968a02bf9dc79e
Aryan Sai <aryansaiarvapelly@gmail.com>
2024-03-13 13:15:05 +0530

    2485

--- Try.java
+++ Try.java
@@ -1,23 +1,6 @@
 
 public class Try {
     public static void main(String[] args) {
-        String s = "cabaabac";
-        StringBuilder sb = new StringBuilder(s);
-        // sb.deleteCharAt(0);
-        int l = 0, u = sb.length() - 1;
-        System.out.println(sb.charAt(l));
-        System.out.println(sb.charAt(u));
-
-        while (sb.charAt(l) == sb.charAt(u)) {
-            sb.deleteCharAt(l);
-            sb.deleteCharAt(u);
-        }
-        System.out.println(sb.toString());
-        System.out.println(sb.length());
-
+       
     }
 }
-
-// for (int num : sol) {
-// System.out.println(num);
-// }
