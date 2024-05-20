bbc89dfca65171cdb8e020b50747464189b9b9c6
Aryan Sai Arvapelly <aryansaiarvapelly@gmail.com>
2024-05-20 19:53:49 +0530

    subsets

---
+++ Done/P1863.java
@@ -0,0 +1,24 @@
+
+public class P1863 {
+    public static void main(String[] args) {
+        int[] nums = {1, 3};
+        System.out.println(generateSubsets(nums));
+    }
+
+    public static int generateSubsets(int[] nums) {
+        int n = nums.length;
+        int numOfSubsets = 1 << n; 
+        int sum = 0,xor;
+        for (int i = 0; i < numOfSubsets; i++) {
+            xor = 0; 
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    xor^=nums[j];
+                }
+            }
+            sum+=xor;
+        }
+        return sum;
+    }
+}
+
